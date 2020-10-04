718f2a318aa4cfe307a1bead411dee1e465f8a18
Mathias <mathiasmichaelweiss@gmail.com>
2020-10-04 14:37:26 +0200

    add_Babel

--- js/modules/tabs.js
+++ js/modules/tabs.js
@@ -1,10 +1,10 @@
 function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
-	let tabs = document.querySelectorAll(tabsSelector),
-		tabsContent = document.querySelectorAll(tabsContentSelector),
-		tabsParent = document.querySelector(tabsParentSelector);
+    let tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(tabsContentSelector),
+        tabsParent = document.querySelector(tabsParentSelector);
 
-	function hideTabContent() {
-        
+    function hideTabContent() {
+
         tabsContent.forEach(item => {
             item.classList.add('hide');
             item.classList.remove('show', 'fade');
@@ -13,27 +13,27 @@ function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass
         tabs.forEach(item => {
             item.classList.remove(activeClass);
         });
-	}
+    }
 
-	function showTabContent(i = 0) {
+    function showTabContent(i = 0) {
         tabsContent[i].classList.add('show', 'fade');
         tabsContent[i].classList.remove('hide');
         tabs[i].classList.add(activeClass);
     }
-    
+
     hideTabContent();
     showTabContent();
 
-	tabsParent.addEventListener('click', function(event) {
+    tabsParent.addEventListener('click', function (event) {
         const target = event.target;
-		if(target && target.classList.contains(tabsSelector.slice(1))) {
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
             tabs.forEach((item, i) => {
                 if (target == item) {
                     hideTabContent();
                     showTabContent(i);
                 }
             });
-		}
+        }
     });
 }
 
